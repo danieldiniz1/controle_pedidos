530126e88ba7ee0da31ea40bc9cf928873b5a01b
Daniel Diniz <daniel.diniz1@icloud.com>
2022-09-27 18:06:31 -0300

    implementado model de estado e cidades, e seus relacionamentos

--- src/main/java/br/com/controle/pedidos/PedidosApplication.java
+++ src/main/java/br/com/controle/pedidos/PedidosApplication.java
@@ -1,8 +1,12 @@
 package br.com.controle.pedidos;
 
 import br.com.controle.pedidos.model.Categoria;
+import br.com.controle.pedidos.model.Cidade;
+import br.com.controle.pedidos.model.Estado;
 import br.com.controle.pedidos.model.Produto;
 import br.com.controle.pedidos.service.CategoriaService;
+import br.com.controle.pedidos.service.CidadeService;
+import br.com.controle.pedidos.service.EstadoService;
 import br.com.controle.pedidos.service.ProdutoService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -25,6 +29,10 @@ public class PedidosApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoService produtoService;
+	@Autowired
+	private EstadoService estadoService;
+	@Autowired
+	private CidadeService cidadeService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);
@@ -40,11 +48,29 @@ public class PedidosApplication implements CommandLineRunner {
 		Produto computador = Produto.novoProduto("Computador", BigDecimal.valueOf(2000.50), Arrays.asList(informatica));
 		Produto impressora = Produto.novoProduto("Impressora", BigDecimal.valueOf(800.67), Arrays.asList(escritorio,informatica));
 		Produto mouse = Produto.novoProduto("Mouse", BigDecimal.valueOf(100.20), Arrays.asList(informatica));
-		produtoService.salvarListaProdutos(Arrays.asList(computador,impressora,mouse));
 
 		informatica.setProdutos(Arrays.asList(computador,impressora,mouse));
 		escritorio.setProdutos(Arrays.asList(mouse));
 
+		produtoService.salvarListaProdutos(Arrays.asList(computador,impressora,mouse));
 		categoriaService.salvarListaCategorias(Arrays.asList(informatica,escritorio));
+
+		Estado sp = Estado.valueOf("SP");
+		Estado mg = Estado.valueOf("MG");
+
+		Cidade campinas = Cidade.valueOf("Campinas", sp);
+		Cidade saoPaulo = Cidade.valueOf("São Paulo", sp);
+		Cidade uberlandia = Cidade.valueOf("Uberlandia", mg);
+
+		sp.adicionarCidade(campinas);
+		sp.adicionarCidade(saoPaulo);
+		mg.adicionarCidade(uberlandia);
+
+//		estadoService.salvarEstado(sp);
+//		estadoService.salvarEstado(mg);
+
+		estadoService.salvarEstados(Arrays.asList(mg,sp));
+		cidadeService.salvarCidades(Arrays.asList(campinas,saoPaulo,uberlandia));
+
 	}
 }
---
+++ src/main/java/br/com/controle/pedidos/service/CidadeService.java
@@ -0,0 +1,9 @@
+package br.com.controle.pedidos.service;
+
+import br.com.controle.pedidos.model.Cidade;
+
+import java.util.List;
+
+public interface CidadeService {
+    void salvarCidades(List<Cidade> cidades);
+}
