fb2c398dc61ac98b85f9ede5d21e1f465ca91797
Daniel Diniz <daniel.diniz1@icloud.com>
2022-09-26 22:57:33 -0300

    model de categoria criado e funcionando

---
+++ src/main/java/br/com/controle/pedidos/model/Categoria.java
@@ -0,0 +1,55 @@
+package br.com.controle.pedidos.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity(name = "categorias")
+public class Categoria {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public static Categoria valueOf(Long id,String nome){
+        return new Categoria(id,nome);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria categoria)) return false;
+        return getId().equals(categoria.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
