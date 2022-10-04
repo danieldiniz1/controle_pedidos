d0c35df5531c62a1b19403b1d9ee485fc66777b9
Daniel Diniz <daniel.diniz1@icloud.com>
2022-10-03 21:15:38 -0300

    ajuste DTO de categorias

--- src/main/java/br/com/controle/pedidos/controller/dto/ListaCategoriasDTO.java
+++ src/main/java/br/com/controle/pedidos/controller/dto/ListaCategoriasDTO.java
@@ -2,28 +2,32 @@ package br.com.controle.pedidos.controller.dto;
 
 import br.com.controle.pedidos.model.Categoria;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ListaCategoriasDTO {
 
-    private List<Categoria> categorias;
+    private Map<String,String> categorias;
 
     public ListaCategoriasDTO() {
     }
 
-    public ListaCategoriasDTO(List<Categoria> categorias) {
+    public ListaCategoriasDTO(HashMap<String, String> categorias) {
         this.categorias = categorias;
     }
 
-    public List<Categoria> getCategorias() {
+    public Map<String, String> getCategorias() {
         return categorias;
     }
 
-    public void setCategorias(List<Categoria> categorias) {
+    public void setCategorias(HashMap<String, String> categorias) {
         this.categorias = categorias;
     }
 
     public static ListaCategoriasDTO valueOf(List<Categoria> categorias){
-        return new ListaCategoriasDTO(categorias);
+        HashMap<String,String> categoriaMap = new HashMap<>();
+        categorias.forEach(categoria -> categoriaMap.put(categoria.getId().toString(),categoria.getNome()));
+        return new ListaCategoriasDTO(categoriaMap);
     }
 }
